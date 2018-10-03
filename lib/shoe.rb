4e0e9b4f3b179f53adab8ec0f38b1c9a12a0c0a9
Beatrice Mendoza <mendoza@nyu.edu>
2018-10-03 02:55:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,11 +3,7 @@ class Shoe
     @brand = brand
   end
   
-  attr_reader :brand
-  attr_accessor :color
-  attr_accessor :size
-  attr_accessor :material
-  attr_accessor :condition
+  attr_reader :brand, :color, :size, :material, :condition
   
   def cobble
     puts "Your shoe is as good as new!"
